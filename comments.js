3c35ed1a6ba0bcc1d17140008dbd1f7db327d8af
Ayush Mokal <95966154+ayushmokal@users.noreply.github.com>
2024-05-28 02:51:43 +0530

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,3 @@
+// Create web server        
+
+// The following code is used to create a web server. This could involve setting up an HTTP server using Node.js's http module, or using a framework like Express.js. The server will listen for incoming requests and send responses.
